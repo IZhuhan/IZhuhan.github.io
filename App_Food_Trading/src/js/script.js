48c4c22c9ece857b3a0885b2768c08e93aafb547
Iryna Zhuhan <zhugan.irina@gmail.com>
2020-09-10 17:06:18 +0300

    fix mobile version of  app_food_trading proj

--- App_Food_Trading/src/js/script.js
+++ App_Food_Trading/src/js/script.js
@@ -1,22 +1,39 @@
 window.addEventListener('load', function() {
   const mainContainer = document.getElementsByClassName('main-container')[0];
+  const inner = document.querySelector('.inner');
+  const logoContainer = document.querySelector('.logo');
   const sectionsWrap = document.querySelector('.sections-wrap');
   const sectionTitles = document.querySelectorAll('.section-title');
   const sections = document.querySelectorAll('.section');
   const sectionsArray = Array.prototype.slice.call(sections);
   const backToTopBtn = document.querySelector('.back-to-top');
 
+  if (document.documentElement.clientWidth < 768) {
+    inner.style.height = `${document.documentElement.clientHeight}px`;
+  }
+
   mainContainer.classList.add('show-container');
 
   const innerTransitions = setTimeout(() => {
-    mainContainer.classList.add('show-content');
-    mainContainer.addEventListener('transitionend', () => clearTimeout(innerTransitions));
-  }, 3000);
+    if (document.documentElement.clientWidth > 767) {
+      mainContainer.classList.add('show-content');
+    } else {
+      inner.style.height = `${logoContainer.clientHeight}px`;
+      inner.style.transition = 'height 1s, transform 1s ease-in';
+    }
 
+    mainContainer.addEventListener('transitionend', () => {
+      clearTimeout(innerTransitions);
+    });
+  }, 3000);
 
   // Scroll to top btn
   backToTopBtn.addEventListener('click', () => {
-    sectionsWrap.scrollTo({ top: 0, behavior: 'smooth' });
+    if (document.documentElement.clientWidth > 767) {
+      sectionsWrap.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   });
 
   // Check if all sections are closed
@@ -43,4 +60,16 @@ window.addEventListener('load', function() {
       checkSections();
     });
   });
+
+  window.addEventListener('resize', (event) => {
+    if (document.documentElement.clientWidth < 768) {
+      mainContainer.classList.remove('show-content');
+
+      inner.style.height = `${logoContainer.clientHeight}px`;
+      inner.style.transition = 'height 1s, transform 1s ease-in';
+    } else {
+      inner.style.height = '100%';
+      mainContainer.classList.add('show-content');
+    }
+  })
 });
